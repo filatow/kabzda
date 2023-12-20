4376d66d67d3cb175ef67202f2c8fe916dddac21
evgeniy <not-found@inbox.ru>
2023-12-20 23:52:39 +0300

    Did UncontrolledRating component

---
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+type UncontrolledRatingPropsType = {}
+
+type StarPropsType = {
+    selected: boolean
+}
+
+function UncontrolledRating(props: UncontrolledRatingPropsType) {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <Star selected={value > 0}/><button onClick={() => setValue(1)}>1</button>
+            <Star selected={value > 1}/><button onClick={() => setValue(2)}>2</button>
+            <Star selected={value > 2}/><button onClick={() => setValue(3)}>3</button>
+            <Star selected={value > 3}/><button onClick={() => setValue(4)}>4</button>
+            <Star selected={value > 4}/><button onClick={() => setValue(5)}>5</button>
+        </div>
+    );
+}
+
+function Star(props: StarPropsType) {
+    if (props.selected) {
+        return <span><b> star</b> </span>;
+    }
+    return <span> star </span>;
+}
+
+export default UncontrolledRating;
