044ca9f37d373cdefb60b7747d34da23a7268b28
filatow <not-found@inbox.ru>
2024-11-21 18:13:04 +0300

    Add code according to videos 13

--- src/components/InputExperiments/Input.stories.tsx
+++ src/components/InputExperiments/Input.stories.tsx
@@ -1,8 +1,12 @@
-import {ChangeEvent, useRef, useState} from 'react';
+import {ChangeEvent, useRef, useState} from 'react'
+import {action} from '@storybook/addon-actions'
 
 export default {
   title: 'Input',
 }
+
+const inputChange = action('Want to change')
+
 
 export const Uncontrolled = () => <input type="text"/>
 
@@ -10,7 +14,7 @@ export const ControlledWithFixedValue = () => <input type="text" value={'constan
 
 export const UncontrolledWithTrackableValue = () => {
   const [value, setValue] = useState('')
-  const handleInputChange = (e:  ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
 
   return <>
     <input type="text" onChange={handleInputChange}/>
@@ -31,4 +35,43 @@ export const UncontrolledTrackedByUseRef = () => {
     <button onClick={() => handleButtonClick()}>set value</button>
     - value: {value}
   </>
+}
+
+export const ControlledInput = () => {
+  const [value, setValue] = useState('')
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+
+  return <input type="text" value={value} onChange={handleInputChange}/>
+}
+
+export const ControlledCheckbox = () => {
+  const [isChecked, setIsChecked] = useState(false)
+  const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.currentTarget.checked)
+  }
+
+  return <label>
+    <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
+    NDA
+  </label>
+}
+
+
+const changeSelect = action('Want to change select')
+export const ControlledSelect = () => {
+  const [value, setValue] = useState<string | undefined>()
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.currentTarget.value)
+  }
+
+  return(
+    <select value={value} onChange={handleSelectChange}>
+      <option>none</option>
+      <option value="first">1</option>
+      <option value="second">2</option>
+      <option value="third">3</option>
+    </select>
+  )
 }
