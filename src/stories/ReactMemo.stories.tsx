b11764265bcdf1478dc9f62b822fe8c8956e281e
filatow <not-found@inbox.ru>
2024-11-24 19:31:26 +0300

    Add code according to videos 17

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+export default {
+  title: 'React.memo demo'
+}
+
+const NewMessagesCounter = (props: { count: number }) => {
+  console.log('render NewMessagesCounter')
+
+  return <div>{props.count}</div>
+}
+
+const Users = React.memo(
+  (props: { users: Array<string> }) => {
+    console.log('render Users')
+    return (
+      <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+      </div>)
+  }
+)
+
+export const Example1 = () => {
+  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState(['Dimych', 'Valera', 'Sveta'])
+
+  const addUser = () => {
+    setUsers([...users, 'Nadya ' + new Date().getTime()])
+  }
+
+  return <>
+    <button onClick={() => setCount(count + 1)}>+</button>
+    <button onClick={() => addUser()}>add user</button>
+    <NewMessagesCounter count={count}/>
+    <Users users={users}/>
+  </>
+}
